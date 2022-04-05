88c90b0864cdc1df161ccbf0a3eafb95e1378b2c
Courtney Zavaleta <courtneyzavaleta@192.168.1.168>
2022-04-05 15:09:40 +0100

    form outline done

--- application/views/pages/form.php
+++ application/views/pages/form.php
@@ -1,13 +1,54 @@
 <h1>Form</h1>
 
-<form>
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" placeholder="Full Name" />
-  </div>
+<div class="container">
+  <form>
+    <!--name-->
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="Full Name" />
+    </div>
 
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" placeholder="Email" />
-  </div>
-</form>
+    <!--email-->
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" class="form-control" id="email" placeholder="Email" />
+    </div>
+
+    <!--how did you hear about us - dropdwon -->
+    <div class="form-group">
+      <label for="heardAboutUsFrom">How did you hear about us?</label>
+      <select class="form-control" id="heardAboutUsFrom">
+        <option>Google</option>
+        <option>Instagram</option>
+        <option>Twitter</option>
+        <option>Facebook</option>
+      </select>
+    </div>
+
+    <!--medium - multiselect -->
+    <div class="form-group">
+      <label for="medium">What is your preferred medium? You may select more than one.</label>
+      <select multiple class="form-control" id="medium">
+        <option>Painting</option>
+        <option>Sculpture</option>
+        <option>Drawing</option>
+        <option>Print</option>
+        <option>Photography</option>
+        <option>Digital</option>
+      </select>
+      <small><i>**hold crtl for Windows or command for Mac to select multiple</i></small>
+    </div>
+
+    <!--img upload - file -->
+    <div class="form-group">
+      <label for="imgs">Upload a sample piece here: </label>
+      <input type="file" class="form-control-file" id="imgs" />
+    </div>
+
+    <!--additional comments - textarea -->
+    <div class="form-group">
+      <label for="textarea">Anything else you'd like to let us know?</label>
+      <textarea class="form-control" id="textarea" rows="2"></textarea>
+    </div>
+  </form>
+</div>
